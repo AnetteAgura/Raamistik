6c9ff56da70d4524ee5da580497da31bcd457d9b
Anette <anetteagura@gmail.com>
2021-04-22 16:43:17 +0300

    Lisatud kasutusele rakendusega seotud konstandid.

---
+++ app/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+// project name
+define('PROJECT_NAME', 'Raamistik');
+// app root dir
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://' . $_SERVER['HTTP_HOST'] . '/' . PROJECT_NAME);
