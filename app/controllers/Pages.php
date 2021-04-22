a6cdb178b58e670da9e5c5f5a2f49a6052f9536b
Anette <anetteagura@gmail.com>
2021-04-22 16:12:08 +0300

    Määratud ja võetud kasutusele kontrolleri meetod ning võimalikud parameetrid.

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -11,4 +11,14 @@ class Pages
     {
         echo 'Pages class is loaded <br>';
     }
+
+    public function index()
+    {
+        echo 'index method is loaded<br>';
+    }
+
+    public function about()
+    {
+        echo 'about method is loaded<br>';
+    }
 }
