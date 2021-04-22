f5a2fb507299959ac95691b999bbdea9d077d199
Anette <anetteagura@gmail.com>
2021-04-22 16:55:37 +0300

    Lisatud kasutusele vaade päis ja jalus.

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,1 +1,7 @@
-<p><?php echo $data['content']; ?></p>
+<?php
+require_once APPROOT . '/views/inc/header.php';
+?>
+    <p><?php echo $data['content']; ?></p>
+<?php
+require_once APPROOT . '/views/inc/footer.php';
+?>
